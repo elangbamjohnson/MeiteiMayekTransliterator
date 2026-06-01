513daaa3c2880af6ac2e07b1bda082cf2ad75f23
Johnson Elangbam <elangbamjohnson@gmail.com>
2026-06-01 19:26:47 +0530

    Initial Commit

---
+++ MeiteiMayekTranslator/MeiteiMayekTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeiteiMayekTranslatorApp.swift
+//  MeiteiMayekTranslator
+//
+//  Created by Johnson Elangbam on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MeiteiMayekTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
